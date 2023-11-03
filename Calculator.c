9e6ba4dcf045b981d025087b509b772a0ff6430a
Fek1r <120334021+Fek1r@users.noreply.github.com>
2023-11-03 09:38:26 +0000

    g

--- Calculator.c
+++ Calculator.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    double x, y, result;
+    char op;
+
+    printf("Введите первое число: ");
+    scanf("%lf", &x);
+    printf("Введите операцию (+, -, *, /): ");
+    scanf(" %c", &op);
+    printf("Введите второе число: ");
+    scanf("%lf", &y);
+
+    switch (op) {
+        case '+':
+            result = x + y;
+            break;
+        case '-':
+            result = x - y;
+            break;
+        case '*':
+            result = x * y;
+            break;
+        case '/':
+            if (y != 0) {
+                result = x / y;
+            } else {
+                printf("Ошибка: Деление на ноль\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Ошибка: Недопустимая операция\n");
+            return 1;
+    }
+
+    printf("Результат: %lf\n", result);
+
+    return 0;
+}
 
